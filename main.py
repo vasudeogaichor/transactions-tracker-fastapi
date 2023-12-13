ab29b53d96b0b4afd4f91d91df2f23faaeb91771
Vasudeo Gaichor <vasudeogaichor@gmail.com>
2023-12-13 15:55:47 +0530

    changed startup events

--- main.py
+++ main.py
@@ -1,21 +1,18 @@
-# project_root/main.py
-
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 # from app.routers.item import router as item_router
-# from app.database.db import database
+from app.database.db import database
 
 app = FastAPI()
 
 # Include the routers
 # app.include_router(item_router, prefix="/v1")
 
-# Check if the database is connected before starting the server
-@app.on_event("startup")
-async def startup_db_client():
-    # await database.connect()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await database.connect()
     print('db connected')
-
-@app.on_event("shutdown")
-async def shutdown_db_client():
-    # await database.disconnect()
+    yield
+    await database.disconnect()
     print('db disconnected')
+    
